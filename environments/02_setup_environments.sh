e8c1b11fc59feeb8239a57e4f2b81175089a3534
Josef Kuchař <josef.kuchar267@gmail.com>
2023-12-08 00:28:37 +0100

    More experiments

--- environments/02_setup_environments.sh
+++ environments/02_setup_environments.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-conda env create -f show-1.yaml
-conda env create -f text2video-finetune.yaml
-conda env create -f llm.yaml
+#conda env create -f show-1.yaml
+#conda create -y -n text2video-finetune python=3.10
+#conda run -v -n text2video-finetune pip install -r text2video-finetune.txt
+conda create -y -n stable-video-diffusion python=3.10
+conda run -v -n stable-video-diffusion pip install -r stable-video-diffusion.txt
+#conda env create -f llm.yaml
 # conda env create -f text2video-zero.yaml
 # conda env create -f zeroscope.yaml

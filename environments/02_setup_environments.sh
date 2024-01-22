bce7df974b8130a21506d04be926e1007f31557b
Josef Kuchař <josef.kuchar267@gmail.com>
2024-01-22 13:02:25 +0100

    Everything in one conda env

--- environments/02_setup_environments.sh
+++ environments/02_setup_environments.sh
@@ -6,9 +6,7 @@
 # conda run -v -n stable-video-diffusion pip install -r stable-video-diffusion.txt
 # conda create -y -n show-1 python=3.8
 # conda run -v -n show-1 pip install -r show-1.txt --extra-index-url https://download.pytorch.org/whl/cu117
-conda create -y -n vgen python=3.8
-conda run -v -n vgen pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
-conda run -v -n vgen pip install -r vgen.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-# conda env create -f llm.yaml
+conda create -y -n gen python=3.8
+conda run -v -n gen pip install -r gen.txt
 # conda env create -f text2video-zero.yaml
 # conda env create -f zeroscope.yaml

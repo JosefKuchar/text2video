03aadf2b821f59039b12c86d28aba6cc5f536ee6
Josef Kuchař <josef.kuchar267@gmail.com>
2024-01-22 13:28:59 +0100

    Merge everything into one python 3.10 env and torch 2

--- environments/02_setup_environments.sh
+++ environments/02_setup_environments.sh
@@ -6,7 +6,7 @@
 # conda run -v -n stable-video-diffusion pip install -r stable-video-diffusion.txt
 # conda create -y -n show-1 python=3.8
 # conda run -v -n show-1 pip install -r show-1.txt --extra-index-url https://download.pytorch.org/whl/cu117
-conda create -y -n gen python=3.8
-conda run -v -n gen pip install -r gen.txt
 # conda env create -f text2video-zero.yaml
 # conda env create -f zeroscope.yaml
+conda create -y -n gen python=3.10
+conda run -v -n gen pip install -r gen.txt
